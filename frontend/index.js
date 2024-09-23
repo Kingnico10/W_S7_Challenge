6fed2cec6928bbe62951256538c044a8f5789963
Kingnico10 <deali1990@yahoo.com>
2024-09-23 14:03:47 -0400

    Sprint 7 Completed

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './components/App'
 import './styles/reset.css'
 import './styles/styles.css'
 
+
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
-root.render(<App />)
+root.render(
+<Router>
+     <App />
+</Router>
+)
--- mvpB.test.js
+++ mvpB.test.js
@@ -14,6 +14,26 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [4] sum('1', 2) // returns 3
     [5] sum('10', '3') // returns 13
   */
+     
+    test('[1] throws an error if no arguments are passed', () => {
+      expect(() => sum()).toThrow('pass valid numbers');
+    });
+  
+    test('[2] throws an error if one argument is not a number', () => {
+      expect(() => sum(2, 'seven')).toThrow('pass valid numbers');
+    });
+  
+    test('[3] returns 4 when passed (1, 3)', () => {
+      expect(sum(1, 3)).toBe(4);
+    });
+  
+    test('[4] returns 3 when passed ("1", 2)', () => {
+      expect(sum('1', 2)).toBe(3);
+    });
+  
+    test('[5] returns 13 when passed ("10", "3")', () => {
+      expect(sum('10', '3')).toBe(13);
+    });
 
   /*
   👉 TASK 2 - Integration Testing of HelloWorld component at the bottom of this module
@@ -29,9 +49,9 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [5] renders a text that reads "JavaScript is pretty awesome"
     [6] renders a text that includes "javaScript is pretty" (use exact = false)
   */
-  test('you can comment out this test', () => {
-    expect(true).toBe(false)
-  })
+  //test('you can comment out this test', () => {
+    //expect(true).toBe(false)
+ // })
 })
 
 function sum(a, b) {
